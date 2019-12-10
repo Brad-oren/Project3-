4ead60a9c383c80924cbfd8ebc17203152b25bfc
sasi1529 <sasi1529@colorado.edu>
2019-12-09 20:04:02 -0700

    sev seg 2second

---
+++ sev_seg2.v
@@ -0,0 +1,22 @@
+module sev_seg1(data, display);
+input [3:0] data;
+output reg [7:0]display;
+
+
+always @(*)
+ begin
+    case(data[3:0])
+    3'b000 : display = 8'b11000000; // 0
+    3'b001 : display = 8'b11110001; // L
+    3'b010 : display = 8'b11110001; //L
+    3'b100 : display = 8'b11110001; //L
+    3'b011 : display = 8'b10111001; //R
+    3'b100 : display = 8'b10111001; //R
+    3'b101 : display = 8'b10111001; //R
+    3'b110 : display = 8'b11000000; //0
+   
+    endcase
+    
+end
+
+endmodule
